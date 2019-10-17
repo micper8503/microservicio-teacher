842eea208ffa08565e61e478f37cb4b505c796e5
micper8503 <michaela-perezp@unilibre.edu.co>
2019-10-17 12:21:54 -0500

    primer commit

---
+++ microservicios-teacher/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Mauricio Alonso', 'Gestion de servicio de Informatica', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Jorge Berbeo', 'Ingenieria Economica', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Fabian Blanco', 'Seguridad Informatica', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Christian Goez', 'Astronomia y Cosmologia', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Oscar Mellizo', 'Ingenieria de Software III', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Oscar Mellizo', 'Programacion en Ambientes WEB', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Jesus Niño', 'Etica', NOW());
+INSERT INTO profesores (name, subject1, creation_date) VALUES ('Carlos Plazas', 'Energia Renovables Alternativas', NOW());
